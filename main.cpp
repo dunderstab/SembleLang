1258a9f0d0460362e099a3b1a19215d5c7faa9d7
dunderstab <79947495+dunderstab@users.noreply.github.com>
2021-04-29 17:52:52 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char** argv) {
     if (argc >= 2) {
         char* filename = argv[1];
         std::string fn(filename);
-        std::string begin = "python3 main.py ";
+        std::string begin = "python3 ~/.semble/main.py ";
         system((begin + fn).c_str());
         exit(0);
     } else {
